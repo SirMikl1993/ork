7661e75b3770bb0d60e85d48f390c929e4800b76
Mikl Aleinikov <sirmikl.aleinikov@gmail.com>
2024-12-10 20:33:06 +0300

    I learn nested routes

---
+++ my-app/src/app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+export default function BlogFirst() {
+    return (
+       <h1>This is first Blog page!</h1>
+     );
+   }
---
+++ my-app/src/app/blog/second/page.tsx
@@ -0,0 +1,5 @@
+export default function BlogSecond() {
+    return (
+       <h1>This is second Blog page!</h1>
+     );
+   }
