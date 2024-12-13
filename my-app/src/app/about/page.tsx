ebcec080dab8ab0622b29aba09acb25d74e17c9e
Admin <sirmikl.aleinikov@gmail.com>
2024-12-13 14:45:07 +0300

    naigation start

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+ title: "About code evolution",
+ description: "About description evolution",
+}
+
 export default function About() {
  return (
     <h1>This is About page!</h1>
--- my-app/src/app/product/[productId]/page.tsx
+++ my-app/src/app/product/[productId]/page.tsx
@@ -1,4 +1,23 @@
-export default function ProductDetails({params}: {params: {productId: string}}) {
+import {Metadata} from "next";
+
+type Props = {
+    params: {
+        productId: string;
+    };
+};
+
+export const generateMetada = async ({params}: Props):Promise <Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(()=>{
+            resolve(`IPhone ${params.productId}`);
+        }, 100)
+    })
+    return {
+        title: `Product ${title}`,
+    };
+};
+
+export default function ProductDetails({params}:Props) {
     return (
         <h1>This is details of product {params.productId}!</h1>
     );
