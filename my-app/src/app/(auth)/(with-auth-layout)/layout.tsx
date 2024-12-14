d8c4d01761887620aa0a1a14aadf10e60e84ca7c
Admin <sirmikl.aleinikov@gmail.com>
2024-12-14 12:30:39 +0300

    23 loading ui

--- my-app/src/app/(auth)/(with-auth-layout)/layout.tsx
+++ my-app/src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -4,6 +4,7 @@ import React from "react"
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 import "./style.css"
+import {useState} from "react";
 
 const navLinks = [
     {name: "Register", href: "/register"},
@@ -13,11 +14,15 @@ const navLinks = [
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
+    const [input, setInput] = React.useState("");
     return (
         <>
-            {
-                navLinks.map((link) => {
-                    const isActive = pathname.startsWith(link.href);
+            <div>
+                <input value={input} onChange={(e) => setInput(e.target.value)}/>
+            </div>
+
+            { navLinks.map((link) => {
+                const isActive = pathname.startsWith(link.href);
                     return (
                         <Link href={link.href} key={link.name} className={isActive?"font-bold mr-4" : "text-blue-500 mr-4"}>{link.name}</Link>
                     );
